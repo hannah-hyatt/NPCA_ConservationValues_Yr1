6490545a59e81a00bb1a52a47cde53bff095ab22
hannah-hyatt <hannah_hyatt@natureserve.org>
2024-02-22 08:45:07 -0800

    minor updates

--- DeepDives/Threshold_HighestConVal/2-1_TabulateArea_HighestConVal_SouthApp.py
+++ DeepDives/Threshold_HighestConVal/2-1_TabulateArea_HighestConVal_SouthApp.py
@@ -1,12 +1,12 @@
-# IUCN analysis for study areas
+# Highest Conservation Value Analysis
 # Before this script:
 # flattened PADUS layer was unioned to Study Areas which were unioned to CONUS (CONUS_AnalysisLayer below)
 # Flags for inside/outside of study areas (field = "StudyArea"), managed/unmanaged lands (field = "managed"), protected/unprotected lands(field = "prot")
 # merged flags with GAP status or Manager Name and name of study areas in format fitting for R code (location of R code here)
 
 # Before this script pt 2:
-# calculated the top 10% quantile of the conservation value in Southern Appalachian study area (conservation value = 15)
-# created a binary raster of that value inside Southern Appalachian study area
+# calculated the top 10% quantile of the conservation value in the study area
+# created a binary raster of the top 10% of values inside study area
 
 # Details on script below:
 # Tabulate area for the field which outlines if the lands are protected, what status of protection they hold, and if it falls inside or outside of study areas.
@@ -23,15 +23,15 @@ arcpy.CheckOutExtension('Spatial')
 arcpy.env.overwriteOutput = True
 
 #set variables
-SA_AnalysisLayer = r"S:\Projects\NPCA\Data\Intermediate\SouthernAppalachianDeepDive.gdb\StudyAreas_PADUS_CONUS_AnalysisLayer_Sapp"
-SA_ConVal = r"S:\Projects\NPCA\Data\Intermediate\ConservationValue\ConVal_Sapp_Binary_V2.tif"
-IUCN_CONUS = r"S:\Projects\_Workspaces\Hannah_Hyatt\ForestEndowment\Data\Intermediate.gdb\IUCNecosystems_CONUS"
-TabAreaGAP_out = r"S:\Projects\NPCA\Data\Intermediate\GAP_Analysis_Species.gdb\TabArea_SAconcal_GAPstatus_V2"
-TabAreaMang_out = r"S:\Projects\NPCA\Data\Intermediate\GAP_Analysis_Species.gdb\TabArea_SAconval_ManagedLands_V2"
+AnalysisLayer = r"S:\Projects\NPCA\Data\Intermediate\GAP_Analysis.gdb\StudyAreas_PADUS_CONUS_AnalysisLayerV2"
+HighestConVal = r"S:\Projects\NPCA\_Year2\Data\Intermediate\ConservationValue_MaskedStudyAreas\HighestConVal_top10pct_GreaterYellowstone.tif"
+#IUCN_CONUS = r"S:\Projects\_Workspaces\Hannah_Hyatt\ForestEndowment\Data\Intermediate.gdb\IUCNecosystems_CONUS"
+TabAreaGAP_out = r"S:\Projects\NPCA\_Year2\Data\Intermediate\TabulateAreaTables_yr2.gdb\TabArea_YellowstoneHighestConVal_GAPsts"
+TabAreaMang_out = r"S:\Projects\NPCA\_Year2\Data\Intermediate\TabulateAreaTables_yr2.gdb\TabArea_YellowstoneHighestConVal_MANGsts"
 print ("variables set")
 
 ## Tabulate Area of PADUS managers within all study areas
-arcpy.sa.TabulateArea(SA_AnalysisLayer, "Mang_NS", SA_ConVal, "Value", TabAreaMang_out, IUCN_CONUS, "CLASSES_AS_ROWS")
+arcpy.sa.TabulateArea(AnalysisLayer, "Mang_NS", SA_ConVal, "Value", TabAreaMang_out, IUCN_CONUS, "CLASSES_AS_ROWS")
 print ("tabulate area managed lands inside areas of highest conservation value in Southern App complete")
 
 ## Tabulate Area of PADUS GAP lands within all study areas
